d417d3a941ed4d56f56d916e2619e30078a90900
1234abhishekjoshi <abhishek8joshi@gmail.com>
2018-11-30 13:46:08 +0530

    changes

--- src/app/service/http.service.ts
+++ src/app/service/http.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable,EventEmitter } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import {environment} from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-
+	signup: EventEmitter<any> = new EventEmitter<any>();	
   constructor(private http:HttpClient) { }
+  createUser(data){
+  		
+  	this.http.post(environment.baseurl+'create_user.php',JSON.stringify({'name':data.fullname,'email':data.email,'gender':data.gender,'password':data.password})).subscribe((res)=>{
+  		this.signup.emit(res);
+  	});
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../service/http.service';
 declare var $ : any;
 @Component({
   selector: 'app-user',
@@ -7,11 +8,27 @@ declare var $ : any;
 })
 export class UserComponent implements OnInit {
  model : any = {};
-  constructor() { }
+  constructor(private http : HttpService) { }
 
   ngOnInit() {
   }
   createUser(){
-  	
+  		 var re = /\S+@\S+\.\S+/;
+     		var rest = re.test(this.model.email)
+  	if(this.model.fullname != 'undefined' && this.model.password != 'undefined' && this.model.email != 'undefined' && this.model.gender != 'undefined' && this.model.password == this.model.rpassword && rest==true){
+  		this.http.createUser(this.model);
+  		this.http.signup.subscribe((res)=>{
+  			if(res.status){
+  				$('.alert-danger').hide();
+  				$('.alert-success').show();
+  				$('.alert-success').html(res.message);
+  			}
+  			else{
+  				$('.alert-success').hide();
+  				$('.alert-danger').show();
+  				$('.alert-danger').html(res.message);
+  			}
+  		});
+  	}
   }
 }
